4291b4f606401f50cb60c74ba4670c69a14e836f
Leandro Lobo <43631792+bigolho16@users.noreply.github.com>
2022-12-03 23:27:01 -0400

    Update islucky.js

--- islucky.js
+++ islucky.js
@@ -1,11 +1,11 @@
-Este Script resolve o seguinte desafio:
-Os números dos bilhetes geralmente consistem em um número par de dígitos. Um número de bilhete é considerado sortudo se a soma da primeira metade dos dígitos for igual à soma da segunda metade. Dado um número de bilhete n, determine se é sorte ou não...
+//Este Script resolve o seguinte desafio:
+//Os números dos bilhetes geralmente consistem em um número par de dígitos. Um número de bilhete é considerado sortudo se a soma da primeira metade dos dígitos for igual à soma da segunda metade. Dado um número de bilhete n, determine se é sorte ou não...
 
-Exemplo:
-Por n = 1230, a saída deve ser
-solution(n) = true;
-Por n = 239017, a saída deve ser
-solution(n) = false.
+//Exemplo:
+//Por n = 1230, a saída deve ser
+//solution(n) = true;
+//Por n = 239017, a saída deve ser
+//solution(n) = false.
 
 
 
